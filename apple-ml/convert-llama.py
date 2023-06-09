450c4a5f8beb38efe89f27ab1a02950093df35e1
Fulton Browne <fultonbrowne@pm.me>
2023-06-09 12:24:55 -0500

    ye

--- apple-ml/convert-llama.py
+++ apple-ml/convert-llama.py
@@ -1,11 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import coremltools as ct
+import torch
 
 def fromPyTorch(sampleinput, tracedmodel):
     model = ct.convert(
     tracedmodel,
     convert_to="mlprogram",
     inputs=[ct.TensorType(shape=sampleinput.shape)])
-    return
-
-    
+    return model
+# LOAD THE MODEL
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+# TRACE THE MODEL
+model.eval()
+example_input = torch.randint(10000, (768,768))
+print(model)
+traced_model = torch.jit.trace(model, example_input)
+# EXPORT THE MODEL
+out = traced_model(example_input)    
 fromPyTorch(None, None).save("out.mlpackage")
